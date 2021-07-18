54b84d4430f630022ea8e67a356608dfaee28a2d
alexnoelle07 <64166422+alexnoelle07@users.noreply.github.com>
2021-07-17 17:18:19 -0700

    updated dictionary component and css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
-
 import './App.css';
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
-        
       </header>
+      <main>
+        <Dictionary />
+      </main>
+
+      <footer className="text-center">
+        Coded by Alex Noelle
+      </footer>
+      </div>
     </div>
   );
 }
